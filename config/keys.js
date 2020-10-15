37cbaad11266ed22af24d1ad75988fbafa2c4997
maintitles <maintitles@yeah.net>
2020-10-15 20:56:55 +0800

    first commit

---
+++ config/keys.js
@@ -0,0 +1,13 @@
+module.exports = {
+  	// mongoURI:'mongodb://nodevueelement:test1234@ds223756.mlab.com:23756/node-vue-element',
+  	mongoURI:"mongodb+srv://nodeapp:nodeapp1234@cluster0.fsae8.mongodb.net/nodeapp",
+  	// mongoURI: 'mongodb+srv://test:test1234@cluster0-piwgv.mongodb.net/test?retryWrites=true&w=majority',
+	// momgoURI:"mongodb://localhost/wxlt",
+	// mongoURI: 'mongodb://wxapi:test1234@ds123698.mlab.com:23698/wxlt-api',
+	// mongoURI:'mongodb + srv://test:test1234@cluster0.7lzek.mongodb.net/test?retryWrites=true&w=majority',
+	// mongoURI:"mongodb + srv://test:test1234@cluster0.7lzek.mongodb.net/test",
+	//mongoURI:"mongodb + srv://test:test1234@cluster0.7lzek.mongodb.net/test?retryWrites=true&w=majority",
+	// mongoURI:"mongodb+srv://nodeapp:f2Wo8kpE0enMvq9Q@cluster0.fsae8.mongodb.net/nodeapp",
+	// mongoURI:"mongodb+srv://nodeapp:f2Wo8kpE0enMvq9Q@cluster0.fsae8.mongodb.net/nodeapp",
+	secretOrKey: 'secret'
+};
